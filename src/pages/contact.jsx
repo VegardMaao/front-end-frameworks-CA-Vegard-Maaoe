5112d95e5b03b89b7d4b9c19c32876c4aa011860
Vegard M. <vm932648@gmail.com>
2024-11-06 13:20:45 +0100

    changed to useForm from react-hook-form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,63 +1,26 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
+    
+    const { register, handleSubmit } = useForm();
 
-    const [firstName, setFirstName] = useState(``);
-    const [lastName, setLastName] = useState(``);
-    const [email, setEmail] = useState(``);
-    const [message, setMessage] = useState(``);
-
-    function onTextInputChange(event) {
-      const value = event.target.value;
-      const name = event.target.name;
-      switch (name) {
-        case "firstName":
-          setFirstName(value);
-          break;
-
-        case "lastName":
-          setLastName(value);
-          break;
-
-        case "email":
-          setEmail(value);
-          break;
-      
-        case "message":
-          setMessage(value);
-          break;
-
-        default:
-          break;
-      }
+    function onFormSubmit(data) {
+      console.log(data);
     }
 
-    function onFormSubmit(event) {
-      event.preventDefault();
-      const body = {
-        firstName,
-        lastName,
-        email,
-        message
-      };
-
-      // Here I could use a fetch / POST to send my data to the backend. But let's just console log it 
-      
-      console.log(body);
-    }    
-
     return <div>
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
           <h1>Send me a message</h1>
           <label htmlFor="firstName">First Name:</label>
-          <input type="text" name="firstName" value={firstName} onChange={onTextInputChange} placeholder="First Name"/>
+          <input {...register("firstName")}/>
           <label htmlFor="lastName">Last Name:</label>
-          <input type="text" name="lastName" value={lastName} onChange={onTextInputChange} placeholder="Last Name"/>
+          <input {...register("lastName")}/>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" value={email} onChange={onTextInputChange} placeholder="your@mail.com"/>
+          <input {...register("email")}/>
           <label htmlFor="message">Your message:</label>
-          <textarea name="message" id="message" value={message} onChange={onTextInputChange} placeholder="Your message here"></textarea>
-          <button>Submit</button>
+          <textarea {...register("message")}/>
+          <input type="submit"/>
         </form>
     </div>
   }
