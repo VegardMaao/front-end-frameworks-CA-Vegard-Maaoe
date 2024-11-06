47789a7dc4b5b4dcb13682926f9f163157257cdd
Vegard M. <vm932648@gmail.com>
2024-11-06 10:37:58 +0100

    Refractoring API call to a hook

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,55 +1,32 @@
 import { useState, useEffect } from "react";
+import { useGetAPI } from "../api/index.js";
 import {  Link } from "react-router-dom";
 import * as S from "../App.styles.js";
 
 export default function Shop() {
-    const url = `https://v2.api.noroff.dev/online-shop`;
+  const url = `https://v2.api.noroff.dev/online-shop`;
+  const {data, isLoading, isError} = useGetAPI(url);
 
-    const [shopItems, setShopItems] = useState([]);
-
-      // State for holding our loading state
-    const [isLoading, setIsLoading] = useState(false);
-    // State for holding our error state
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => { 
-        async function getData() {
-            try {
-                setIsError(false);   
-                setIsLoading(true); 
-                const response = await fetch(url);
-                const json = await response.json();
-                setShopItems(json.data);
-                setIsLoading(false);
-            } catch (error) {
-                setIsLoading(false);
-                setIsError(true);
-            }
-        }
-        getData();
-     }, [])
-
-     if (isLoading) {
-        return <div>Loading posts</div>;
-      }
+  if (isLoading) {
+            return <div>Loading posts</div>;
+          }
+        
+          if (isError) {
+            return <div>Error loading data</div>;
+          }
     
-      if (isError) {
-        return <div>Error loading data</div>;
-      }
-
-    console.log(shopItems);
-
-    return <div>
-        <h1>Shop:</h1>
-            {shopItems.map((item) => (
-                <S.ShopItem key={item.id}>
-                    <Link to={`/singleItem?id=${item.id}`}>
-                    <S.Thumbnail src={item.image.url} alt={item.image.alt}/>
-                    <h2>{item.title}</h2>
-                    <p>{item.description}</p>
-                    </Link>
-                </S.ShopItem>
-            ))}
-    </div>
-  }
-  
+        console.log(data);
+    
+        return <div>
+            <h1>Shop:</h1>
+                {data.map((item) => (
+                    <S.ShopItem key={item.id}>
+                        <Link to={`/singleItem?id=${item.id}`}>
+                        <S.Thumbnail src={item.image.url} alt={item.image.alt}/>
+                        <h2>{item.title}</h2>
+                        <p>{item.description}</p>
+                        </Link>
+                    </S.ShopItem>
+                ))}
+        </div>  
+};
--- src/pages/singleItem.jsx
+++ src/pages/singleItem.jsx
@@ -1,8 +1,5 @@
-
-const urlParams = new URLSearchParams(window.location.search);
-const idParams = urlParams.get('id');
-
-
 export default function SingleItem() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idParams = urlParams.get('id');
     return <div>{`Single Item ID: ${idParams}`}</div>
 }
