994b42fd71255ab026cbeb0933446bf90d0ec365
Vegard M. <vm932648@gmail.com>
2024-11-08 14:50:42 +0100

    test is mostly working kinda

--- src/pages/singleItem/singleItem.jsx
+++ src/pages/singleItem/singleItem.jsx
@@ -1,4 +1,4 @@
-export default function SingleItem() {
+export default function SingleItem(url) {
     const urlParams = new URLSearchParams(window.location.search);
     const idParams = urlParams.get('id');
     return <div>{`Single Item ID: ${idParams}`}</div>
--- src/pages/singleItem/singleItem.test.js
+++ src/pages/singleItem/singleItem.test.js
@@ -1,42 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import SingleItem from './singleItem';
-
-const oldWindowLocation = window.location;
-
-beforeAll(() => {
-    delete window.location
-  
-    window.location = Object.defineProperties(
-      {},
-      {
-        ...Object.getOwnPropertyDescriptors(oldWindowLocation),
-        assign: {
-          configurable: true,
-          value: jest.fn(),
-        },
-      },
-    )
-  })
-  beforeEach(() => {
-    window.location.assign.mockReset()
-  })
-  afterAll(() => {
-    // restore `window.location` to the `jsdom` `Location` object
-    window.location = oldWindowLocation
-  })
-  
-  
 
 describe('SingleItem', () => {
     test('displays information correctly', () => {
-        window.location.assign('https://www.url.com/?id=1234asdf')
+
+        jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((id) => '1234asdf');
+
         render(
             <SingleItem />,
         );
-    
-        // Displays our DOM for debugging purposes
-        expect(screen.getByText('1234asdf')).toBeInTheDocument();
+        expect(screen.getByText('Single Item ID: 1234asdf')).toBeInTheDocument();
         screen.debug();
         });
   });
-  
