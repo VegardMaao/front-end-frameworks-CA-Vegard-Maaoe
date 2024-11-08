9d9bdfb0e6e8460039afcf02e268e551fd0dc700
Vegard M. <vm932648@gmail.com>
2024-11-08 12:25:41 +0100

    trying to figure out testing

--- src/pages/singleItem/singleItem.jsx
+++ src/pages/singleItem/singleItem.jsx
@@ -1,12 +1,5 @@
-export default function SingleItem(param) {
-    if (!param) {
-        const urlParams = new URLSearchParams(window.location.search);
-        const idParams = urlParams.get('id');
-        return <div>{`Single Item ID: ${idParams}`}</div>
-    } else {
-        const idParams = param;
-        return <div>{`Single Item ID: ${idParams}`}</div>
-    }
-    
-   
+export default function SingleItem() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idParams = urlParams.get('id');
+    return <div>{`Single Item ID: ${idParams}`}</div>
 }
--- src/pages/singleItem/singleItem.test.js
+++ src/pages/singleItem/singleItem.test.js
@@ -1,17 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import SingleItem from './singleItem';
 
+const oldWindowLocation = window.location;
+
+beforeAll(() => {
+    delete window.location
+  
+    window.location = Object.defineProperties(
+      {},
+      {
+        ...Object.getOwnPropertyDescriptors(oldWindowLocation),
+        assign: {
+          configurable: true,
+          value: jest.fn(),
+        },
+      },
+    )
+  })
+  beforeEach(() => {
+    window.location.assign.mockReset()
+  })
+  afterAll(() => {
+    // restore `window.location` to the `jsdom` `Location` object
+    window.location = oldWindowLocation
+  })
+  
+  
+
 describe('SingleItem', () => {
     test('displays information correctly', () => {
-      render(
-        <SingleItem
-        param="1234asdf"
-        />,
-      );
-  
-      // Displays our DOM for debugging purposes
-      expect(screen.getByText('1234asdf')).toBeInTheDocument();
-      screen.debug();
-    });
+        window.location.assign('https://www.url.com/?id=1234asdf')
+        render(
+            <SingleItem />,
+        );
+    
+        // Displays our DOM for debugging purposes
+        expect(screen.getByText('1234asdf')).toBeInTheDocument();
+        screen.debug();
+        });
   });
   
