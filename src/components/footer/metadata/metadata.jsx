463c63ea06a848fea2c069233e405277a639d247
Vegard M. <vm932648@gmail.com>
2024-11-11 12:47:14 +0100

    Update metadata.jsx

--- src/components/footer/metadata/metadata.jsx
+++ src/components/footer/metadata/metadata.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 
 export default function Metadata({title, description} ) {
     return <Helmet>
+    <meta name="author" content="Vegard Maaø" />
     <meta name="description" content={`${description}`} />
     <title>{`${title}`}</title>
     <script src="https://kit.fontawesome.com/efafec28fb.js" crossorigin="anonymous"></script>
