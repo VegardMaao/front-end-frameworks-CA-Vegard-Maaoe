ca780798d0dbf5687a35700a562e1bab3a8a6880
Vegard M. <vm932648@gmail.com>
2024-11-15 13:35:08 +0100

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,9 +2,6 @@ import { Outlet } from "react-router-dom"
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
-
-  // The <Outlet> from react-router-dom displays any child routes, almost like
-  // passing through "children" in a component
   export default function Layout() {
     return (
       <div>
