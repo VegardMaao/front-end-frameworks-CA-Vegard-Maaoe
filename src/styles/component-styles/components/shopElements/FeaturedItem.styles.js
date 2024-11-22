9adbea70244606b30343b8a03f7655bd30d2dd97
Vegard M. <vm932648@gmail.com>
2024-11-22 11:34:13 +0100

    Update FeaturedItem.styles.js

--- src/styles/component-styles/components/shopElements/FeaturedItem.styles.js
+++ src/styles/component-styles/components/shopElements/FeaturedItem.styles.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import { device } from "../../../BreakPoints";
 
 
 
 export const FeaturedItemWrapper = styled.div`
     background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), no-repeat center/100% url(${(props) => props.url});
-    height: 40vh;
+    height: 50vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    @media${device.sm}{
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), no-repeat center url(${(props) => props.url});
+    }
 }
 `;
 
