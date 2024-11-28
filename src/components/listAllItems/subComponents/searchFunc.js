98a896e477af0bc87791250bb411a73b80e872cc
Vegard M. <vm932648@gmail.com>
2024-11-28 13:49:47 +0100

    Update searchFunc.js
    Search was case sensitive and I don't want that. fixed

--- src/components/listAllItems/subComponents/searchFunc.js
+++ src/components/listAllItems/subComponents/searchFunc.js
@@ -1,8 +1,9 @@
 export function LogInp(e, params){
     const {data, dataArray, setDataArray} = params;
     const searchQuery = e.target.value;
+    const lowerCaseSearchQuery = searchQuery.toLowerCase();
 
-    const filtered = data.filter(item => Object.values(item).some(val => typeof val === "string" && val.includes(searchQuery)));
+    const filtered = data.filter(item => Object.values(item).some(val => typeof val === "string" && val.toLowerCase().includes(lowerCaseSearchQuery)));
         
     setDataArray(filtered);
     console.log(dataArray);
