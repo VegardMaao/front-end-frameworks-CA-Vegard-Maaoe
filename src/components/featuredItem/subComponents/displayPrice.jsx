c13f33b590136be17e240ed0bd081b43eb100601
Vegard M. <vm932648@gmail.com>
2024-12-03 10:19:48 +0100

    Update displayPrice.jsx

--- src/components/featuredItem/subComponents/displayPrice.jsx
+++ src/components/featuredItem/subComponents/displayPrice.jsx
@@ -2,9 +2,7 @@ import { featuredItemStyles } from "styles";
 import { useParams } from "react-router-dom";
 
 export function DisplayPrice(params) {
-    const { id } = useParams();
     const {color, discountedPrice, regularPrice} = params;
-    console.log(color)
     if (discountedPrice && discountedPrice != regularPrice) {
         return <featuredItemStyles.FeaturedParagraph colors={color}>
             <featuredItemStyles.LineThrough>{regularPrice}</featuredItemStyles.LineThrough>
