76b8ef2e52f6965f1b3612e2a350f1f88c9381a3
Vegard M. <vm932648@gmail.com>
2024-12-09 13:40:47 +0100

    Update listItemsInCheckout.jsx

--- src/components/checkout/subComponents/listItemsInCheckout.jsx
+++ src/components/checkout/subComponents/listItemsInCheckout.jsx
@@ -2,9 +2,7 @@ import { DisplayPrice } from "../../featuredItem/subComponents/displayPrice";
 import { buttons, shopStyles } from "styles";
 
 export function ListItemsInCheckout(params) {
-
     const {items, removeItem} = params;
-    
     return items.map((item) => (
         <shopStyles.ShopItem key={item.id}>
             <shopStyles.Thumbnail src={item.image.url} alt={item.image.alt}/>
